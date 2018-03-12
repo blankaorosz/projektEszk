9e8e0c55502c04c38e06e4a205e72df5e7389f80
Blanka Orosz <oroszblanki@gmail.com>
2018-03-12 16:07:14 +0100

    example - hello

--- ftdl/src/main/java/hu/elte/ftdl/controller/HelloController.java
+++ ftdl/src/main/java/hu/elte/ftdl/controller/HelloController.java
@@ -3,9 +3,6 @@ package hu.elte.ftdl.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by vimtaai on 2017. 09. 18..
- */
 @RestController
 public class HelloController {
 
